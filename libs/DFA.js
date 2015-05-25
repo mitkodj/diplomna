94d3be8c61646286b93320980e1a21e7eecfd968
Dimitar <radmit@abv.bg>
2015-05-25 09:09:29 +0300

    Minor backup.

--- libs/DFA.js
+++ libs/DFA.js
@@ -3,64 +3,230 @@ var _ = require('lodash');
 var automata = [{
 		name: 'f0',
 		transitions: [{
-			t: '0', s: 'f1'
+			t: 'u', s: 'f1'
 		},{
-			t: '1', s: 'f1'
+			t: '@', s: 'f19'
 		},{
-			t: '2', s: 'f1'
+			t: '0', s: 'f28'
 		},{
-			t: '3', s: 'f1'
+			t: '1', s: 'f28'
 		},{
-			t: '4', s: 'f1'
+			t: '2', s: 'f28'
 		},{
-			t: '5', s: 'f1'
+			t: '3', s: 'f28'
 		},{
-			t: '6', s: 'f1'
+			t: '4', s: 'f28'
 		},{
-			t: '7', s: 'f1'
+			t: '5', s: 'f28'
 		},{
-			t: '8', s: 'f1'
+			t: '6', s: 'f28'
 		},{
-			t: '9', s: 'f1'
+			t: '7', s: 'f28'
+		},{
+			t: '8', s: 'f28'
+		},{
+			t: '9', s: 'f28'
 		}],
 		failTransition: 'f0'
-	}, {
+	},{
 		name: 'f1',
 		transitions: [{
-			t: '=', s: 'f2'
+			t: 'n', s: 'f2'
+		},{
+			t: 's', s: 'f14'
 		}],
 		failTransition: 'f0'
-	}, {
+	},{
 		name: 'f2',
 		transitions: [{
-			t: '0', s: 'f3'
-		},{
-			t: '1', s: 'f3'
-		},{
-			t: '2', s: 'f3'
-		},{
-			t: '3', s: 'f3'
-		},{
-			t: '4', s: 'f3'
-		},{
-			t: '5', s: 'f3'
-		},{
-			t: '6', s: 'f3'
-		},{
-			t: '7', s: 'f3'
-		},{
-			t: '8', s: 'f3'
-		},{
-			t: '9', s: 'f3'
+			t: 'i', s: 'f3'
 		}],
 		failTransition: 'f0'
-	}, {
+	},{
 		name: 'f3',
+		transitions: [{
+			t: 'o', s: 'f4'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f4',
+		transitions: [{
+			t: 'n', s: 'f5'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f5',
+		transitions: [{
+			t: ' ', s: 'f5'
+		},{
+			t: 'a', s: 'f6'
+		},{
+			t: 's', s: 'f8'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f6',
+		transitions: [{
+			t: 'l', s: 'f7'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f7',
+		transitions: [{
+			t: 'l', s: 'f5'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f8',
+		transitions: [{
+			t: 'e', s: 'f9'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f9',
+		transitions: [{
+			t: 'l', s: 'f10'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f10',
+		transitions: [{
+			t: 'e', s: 'f11'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f11',
+		transitions: [{
+			t: 'c', s: 'f12'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f12',
+		transitions: [{
+			t: 't', s: 'f3'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f13',
+		transitions: [],
+		failTransition: 'f0'
+	},{
+		name: 'f14',
+		transitions: [{
+			t: 'e', s: 'f15'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f15',
+		transitions: [{
+			t: 'r', s: 'f16'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f16',
+		transitions: [{
+			t: '(', s: 'f17'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f17',
+		transitions: [{
+			t: ')', s: 'f18'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f18',
+		transitions: [],
+		failTransition: 'f0'
+	},{
+		name: 'f19',
+		transitions: [{
+			t: '@', s: 'f20'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f20',
+		transitions: [{
+			t: 'v', s: 'f21'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f21',
+		transitions: [{
+			t: 'e', s: 'f22'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f22',
+		transitions: [{
+			t: 'r', s: 'f23'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f23',
+		transitions: [{
+			t: 's', s: 'f24'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f24',
+		transitions: [{
+			t: 'i', s: 'f25'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f25',
+		transitions: [{
+			t: 'o', s: 'f26'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f26',
+		transitions: [{
+			t: 'n', s: 'f27'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f27',
+		transitions: [],
+		failTransition: 'f0'
+	},{
+		name: 'f28',
+		transitions: [{
+			t: '=', s: 'f29'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f29',
+		transitions: [{
+			t: '0', s: 'f30'
+		},{
+			t: '1', s: 'f30'
+		},{
+			t: '2', s: 'f30'
+		},{
+			t: '3', s: 'f30'
+		},{
+			t: '4', s: 'f30'
+		},{
+			t: '5', s: 'f30'
+		},{
+			t: '6', s: 'f30'
+		},{
+			t: '7', s: 'f30'
+		},{
+			t: '8', s: 'f30'
+		},{
+			t: '9', s: 'f30'
+		}],
+		failTransition: 'f0'
+	},{
+		name: 'f30',
 		transitions: [],
 		failTransition: 'f0'
 	}];
 
-var finalStates = ['f3'];
+var finalStates = ['f13', 'f18', 'f27', 'f30'];
 
 var currentState = automata[0];
 
--- routes/bank.js
+++ routes/bank.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var log4js = require('log4js');
 var jade = require('jade');
 
-var bank = require('../libs/bank');
+var banks = require('../libs/bank');
+var users = require('../libs/users');
 router.post('/', function(req, res) {
   console.log(req.body);
   bank.getBankData(req.body.iban)
   .then(function(rows) {
     console.log(rows);
+
+    if (rows == "Attempted SQL Injection.") {
+    	users.saveUserIP(banks.currentUser);
+    }
     res.send(rows);
     // if (rows.length == 1){
     //   var htmlOutput = {
