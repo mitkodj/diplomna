99b6583e2b850fe1582dd3034d6808ec49fd2b61
Dimitar <radmit@abv.bg>
2015-06-07 22:59:42 +0300

    Developing the test tool.

--- routes/testTool.js
+++ routes/testTool.js
@@ -32,23 +32,69 @@ router.get('/test', function(req, res) {
         "1 UNION SELECT @@hostname, 1, 1", 
 	];
 
+    var users = [{
+        Id: 1,
+        username: 'mitko',
+        password: 'dimitar'
+    }, {
+        Id: 2,
+        username: 'mira',
+        password: '12345'
+    }, {
+        Id: 3,
+        username: 'ivan',
+        password: '0000'
+    }, {
+        Id: 4,
+        username: 'test_user',
+        password: 'test'
+    }];
+
+    var IPs = [
+        '127.0.0.1',
+        '192.167.11.203',
+        '68.191.13.44'
+    ];
+
     var randomNumbers = [];
-    for (var i=0; i < minVal; i++) {
+    for (var i=0; i < 20; i++) {
         randomNumbers.push(Random.integer(0, 3)(Random.engines.nativeMath));
     }
 
-    for (i=0; i < 10 - minVal; i++) {
-        randomNumbers.push(Random.integer(0, 10)(Random.engines.nativeMath));
+    var randomData = _.map(randomNumbers, function(element){
+        return users[element];
+    });
+
+    // console.log(randomData);
+
+    randomNumbers = [];
+
+    for (i=0; i < 20; i++) {
+        var index = Random.integer(0, 10)(Random.engines.nativeMath);
+
+        randomData[i].query = requestQueries[index];
+
+        var ip = Random.integer(0, 2)(Random.engines.nativeMath);
+
+        randomData[i].IP = IPs[ip];
+        console.log(i,randomData[i].username, ip, randomData[i].IP);
     }
-    console.log(minVal, 10 - minVal);
 
-    randomNumbers = _.map(randomNumbers, function(element){
-        return requestQueries[element];
-    });
+    console.log('=================');
 
-    async.map(randomNumbers, banks.getBankDataAsync, function (err, results) {
-        res.send(results);
-    });
+    for (i=0; i<20;i++) {
+        console.log(i, randomData[i].username, randomData[i].IP);
+    }
+
+    var groupedCollection = _.groupBy(randomData, 'username');
+
+    // async.map(randomData, function() {
+
+    // }, function (err, results) {
+        // res.send(results);
+    // });
+
+    res.send(groupedCollection);
 });
 
 module.exports = router;
